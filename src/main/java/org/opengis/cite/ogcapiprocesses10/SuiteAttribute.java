8b0ea589e9d61dc6ca8cb782800933e14046b335
DivyaSreeMunagavalasa <divya.sree@datakaveri.org>
2025-08-26 12:11:33 +0530

    Revert unintended formatting changes in SuiteAttribute.java and TestRunArg.java

--- src/main/java/org/opengis/cite/ogcapiprocesses10/SuiteAttribute.java
+++ src/main/java/org/opengis/cite/ogcapiprocesses10/SuiteAttribute.java
@@ -17,57 +17,57 @@ import jakarta.ws.rs.client.Client;
 @SuppressWarnings("rawtypes")
 public enum SuiteAttribute {
 
-    /**
-     * A client component for interacting with HTTP endpoints.
-     */
-    CLIENT("httpClient", Client.class),
+	/**
+	 * A client component for interacting with HTTP endpoints.
+	 */
+	CLIENT("httpClient", Client.class),
 
-    /**
-     * The root URL.
-     */
-    IUT("instanceUnderTest", URI.class),
+	/**
+	 * The root URL.
+	 */
+	IUT("instanceUnderTest", URI.class),
 
-    /**
-     * A File containing the test subject or a description of it.
-     */
-    TEST_SUBJ_FILE("testSubjectFile", File.class),
+	/**
+	 * A File containing the test subject or a description of it.
+	 */
+	TEST_SUBJ_FILE("testSubjectFile", File.class),
 
-    /**
-     * The number of collections to test.
-     */
-    NO_OF_COLLECTIONS("noOfCollections", Integer.class),
+	/**
+	 * The number of collections to test.
+	 */
+	NO_OF_COLLECTIONS("noOfCollections", Integer.class),
 
-    /**
-     * The id of the echo process.
-     */
-    ECHO_PROCESS_ID("echoProcessId", String.class),
+	/**
+	 * The id of the echo process.
+	 */
+	ECHO_PROCESS_ID("echoProcessId", String.class),
 
-    /**
-     * Boolean indicating whether all processes should be tested against the OGC Process
-     * Description Conformance Class.
-     */
-    TEST_ALL_PROCESSES("testAllProcesses", Boolean.class),
+	/**
+	 * Boolean indicating whether all processes should be tested against the OGC Process
+	 * Description Conformance Class.
+	 */
+	TEST_ALL_PROCESSES("testAllProcesses", Boolean.class),
 
-    /**
-     * Number of processes that should be tested against the OGC Process Description
-     * Conformance Class.
-     */
-    PROCESS_TEST_LIMIT("processTestLimit", Integer.class),
+	/**
+	 * Number of processes that should be tested against the OGC Process Description
+	 * Conformance Class.
+	 */
+	PROCESS_TEST_LIMIT("processTestLimit", Integer.class),
 
-    /**
-     * Parsed OpenApi3 document resource /api; Added during execution.
-     */
-    API_MODEL("apiModel", OpenApi3.class),
+	/**
+	 * Parsed OpenApi3 document resource /api; Added during execution.
+	 */
+	API_MODEL("apiModel", OpenApi3.class),
 
-    /**
-     * Use local OpenAPI schema included in ETS.
-     */
-    USE_LOCAL_SCHEMA("useLocalSchema", Boolean.class),
+	/**
+	 * Use local OpenAPI schema included in ETS.
+	 */
+	USE_LOCAL_SCHEMA("useLocalSchema", Boolean.class),
 
-    /**
-     * Requirement classes parsed from /conformance; Added during execution.
-     */
-    REQUIREMENTCLASSES("requirementclasses", List.class),
+	/**
+	 * Requirement classes parsed from /conformance; Added during execution.
+	 */
+	REQUIREMENTCLASSES("requirementclasses", List.class),
 
     /**
      * Authorization token for API requests.
@@ -76,39 +76,39 @@ public enum SuiteAttribute {
 
     private final Class attrType;
 
-    private final String attrName;
+	private final String attrName;
 
-    SuiteAttribute(String attrName, Class attrType) {
-        this.attrName = attrName;
-        this.attrType = attrType;
-    }
+	SuiteAttribute(String attrName, Class attrType) {
+		this.attrName = attrName;
+		this.attrType = attrType;
+	}
 
-    /**
-     * <p>
-     * getType.
-     * </p>
-     * @return a {@link java.lang.Class} object
-     */
-    public Class getType() {
-        return attrType;
-    }
+	/**
+	 * <p>
+	 * getType.
+	 * </p>
+	 * @return a {@link java.lang.Class} object
+	 */
+	public Class getType() {
+		return attrType;
+	}
 
-    /**
-     * <p>
-     * getName.
-     * </p>
-     * @return a {@link java.lang.String} object
-     */
-    public String getName() {
-        return attrName;
-    }
+	/**
+	 * <p>
+	 * getName.
+	 * </p>
+	 * @return a {@link java.lang.String} object
+	 */
+	public String getName() {
+		return attrName;
+	}
 
-    /** {@inheritDoc} */
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder(attrName);
-        sb.append('(').append(attrType.getName()).append(')');
-        return sb.toString();
-    }
+	/** {@inheritDoc} */
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder(attrName);
+		sb.append('(').append(attrType.getName()).append(')');
+		return sb.toString();
+	}
 
 }
